16c35d15ef6ef5c84c9aa9648d1d488c421f6cdc
Abiral <abiralkc21@gmail.com>
2022-07-25 23:08:05 +0545

    Adding main header and navlinks

--- src/App.js
+++ src/App.js
@@ -2,25 +2,28 @@ import React from 'react';
 import {BrowserRouter as Router,Route,Redirect,Switch} from 'react-router-dom';
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/Navigation/MainNavigation';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
+      <MainNavigation/>
+      <main>
+        <Switch>
+          <Route path="/" exact>
 
-          <Users/>
-
-        </Route>
-        <Route path="/places/new" exact>
-
-          <NewPlace/>
+            <Users/>
 
           </Route>
-        <Redirect to="/" />
-    </Switch>
+          <Route path="/places/new" exact>
 
-  </Router>
+            <NewPlace/>
+
+            </Route>
+          <Redirect to="/" />
+        </Switch>
+     </main>
+   </Router>
   );
 }
 
